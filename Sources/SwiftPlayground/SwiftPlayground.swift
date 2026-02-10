f69c84f29c407fcbf22c62ad5feb4ff2cbbef66b
Dylanwastaken12 <142276112+Dylanwastaken12@users.noreply.github.com>
2026-02-10 23:50:00 +0000

    Starting map filter and reduce

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,26 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+        let numbers = [1, 2, 3, 4, 5]
+
+        // cubes numbers in the array
+        let cubedNumbers = numbers.map { number in
+            return number * number * number
+        }
+        print(cubedNumbers)
+
+        // filters out the eve numbers in the new array of cubed numbers
+        let evenNumbers = cubedNumbers.filter { number in
+            return number % 2 == 0
+        }
+        print(evenNumbers)
+
+        // adds the even cubed numbers into one number 
+        let total = evenNumbers.reduce(0) { result, number in
+            return result + number
+        }
+
+        // prints combined value of even cubed numbers
+        print(total)
     }
 }
