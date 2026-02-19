309ca4b671d403fe9d00342c5539e8021cbe9750
Dylanwastaken12 <142276112+Dylanwastaken12@users.noreply.github.com>
2026-02-19 21:59:33 +0000

    finished Task A

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,23 +1,46 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+
+//Practice Task Students:
+
+// struct is created for students showing their information
+struct Student {
+    let ID: Int
+    var name: String
+    let NSN: Int
+    var email: String
+    var age: Int
+
+    // This function prints a summary about a given student
+    func summary() -> String {
+        return "\(name) is \(age) years old, their student ID is \(ID) and their NSN is \(NSN), their email is \(email)."
+    }
+}
+
+
+// This struct is for Task A: race garage
+// Struct contains info about given car models
+struct Car {
+    let brand: String
+    let model: String
+    let year: Int
+
+    // This func prints a summary about a given car
+    func carSummary() -> String {
+        return "The car is a \(brand) \(model) from \(year)"
+    }    
+}
+
+
 @main
 struct SwiftPlayground {
     static func main() {
-        struct Student {
-            let ID: Int
-            var name: String
-            let NSN: Int
-            var email: String
-            var age: Int
-
-            func summary() -> String {
-            return "\(name) is \(age) years old, their student ID is \(ID) and their NSN is \(NSN), their email is \(email)."
-        }
-        }
         
+        // Creates a student for myself containing my info
         let dylan = Student(ID: 22774, name: "Dylan", NSN: 0143802249, email: "dylan.jenkins@student.onslow.school.nz", age: 17)
 
+        // Creates a list of students and their info, all of which are 100% real
         let students: [Student] = [
             Student(ID: 38465, name: "Bartholemew", NSN: 67, email: "bart.tholemew@student.onslow.school.nz", age: 97),
             Student(ID: 94769, name: "Bernard", NSN: 12, email: "ber.nard@student.onslow.school.nz", age: 14),
@@ -26,15 +49,26 @@ struct SwiftPlayground {
             Student(ID: 19642, name: "Alex", NSN: 24, email: "alex.jenkins@student.onslow.school.nz", age: 15)
         ]
         
+        // creates a new list that gets the ages of each student in 10 years using .map
+        // all the names and new ages are then printed
         let agesInADecade = students.map {
             "\($0.name) will be \($0.age + 10) in a decade"
         }
-        print(agesInADecade)
+        print(agesInADecade)        
 
-        print("Dylan is \(dylan.age) years old")
-
-        
-
+        // prints a summary for myself
         print(dylan.summary())
+
+// Task A: race garage:
+
+        // a list is created containing different cars
+        let cars: [Car] = [
+            Car(brand: "Toyota", model: "Corolla", year: 2019),
+            Car(brand: "Toyota", model: "Rav 4", year: 2017)
+        ]
+
+        // the different cars are then printed using the car summary function
+        print(cars[0].carSummary())
+        print(cars[1].carSummary())
     }
 }
