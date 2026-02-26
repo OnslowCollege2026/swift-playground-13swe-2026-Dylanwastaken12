467baa0426fd9eac2b8270619cd81749238dad3d
Dylanwastaken12 <142276112+Dylanwastaken12@users.noreply.github.com>
2026-02-26 22:12:18 +0000

    Started task A SchoolSystem

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,9 +1,55 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
-
+import Foundation
 @main
+
+// SchoolSystem Task.
+
+
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+        
+        
+        do {
+            let enrolment = Enrolment(studentID: 22774, courseID: 70968)
+            let data = try JSONEncoder().encode(enrolment)
+            let decoded = try JSONDecoder().decode(Enrolment.self, from: data)
+            print(data)
+            print(decoded)
+        } catch {
+            print("Error: \(error)")
+        }
+        
+
+        
     }
 }
+
+// Creates a struct for student that uses the protocol "Identifiable" and stores id, name, and age.
+struct Student: Identifiable {
+    let id: String
+    var name: String
+    let age: Int
+}
+
+// Creates a struct called course that uses the protocol CustomStringConvertable
+struct Course: CustomStringConvertible {
+    let id: String
+    var title: String
+    var courseDescription: String
+
+    var description: String {
+        """
+        --- \(title)---
+        CourseID: \(id)
+        \(courseDescription)
+        """
+    }
+}
+
+struct Enrolment: Codable {
+    let studentID: Int
+    let courseID: Int
+}
+
+
