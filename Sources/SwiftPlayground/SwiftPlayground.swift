ed1696dc491a90090f9419303e8198f2625c9d59
Dylanwastaken12 <142276112+Dylanwastaken12@users.noreply.github.com>
2026-03-02 02:05:59 +0000

    Pretty much finished Schoolsystem PArt 2

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -14,14 +14,26 @@ struct SwiftPlayground {
             let enrolment = Enrolment(studentID: 22774, courseID: 70968)
             let data = try JSONEncoder().encode(enrolment)
             let decoded = try JSONDecoder().decode(Enrolment.self, from: data)
-            print(data)
-            print(decoded)
+            print("The encoded data looks like: \(data)")
+            print("The decoded data looks like: \(decoded)")
         } catch {
             print("Error: \(error)")
         }
         
+        // creates a list of student ID's and scores
+        let scores: [ScoreEntry] = [
+            ScoreEntry(studentID: 26396, points: 127),
+            ScoreEntry(studentID: 22774, points: 856),
+            ScoreEntry(studentID: 24249, points: 645)
+        ]
 
-        
+        // sorts the instances by the number of points
+        for item in scores.sorted() {
+            print(item)
+        }
+
+        let foo: Enrolment = Enrolment(studentID: 0, courseID: 17)
+        print(foo.hashValue)
     }
 }
 
@@ -47,9 +59,29 @@ struct Course: CustomStringConvertible {
     }
 }
 
-struct Enrolment: Codable {
+
+struct Enrolment: Codable, Hashable {
     let studentID: Int
     let courseID: Int
+
+    func hash(into hasher: inout Hasher) {
+        // hasher.combine(studentID)
+        // hasher.combine(courseID)
+
+        hasher.combine("\(studentID).\(courseID)")
+    }
 }
 
+// creates a struct that enters scores and student ID's
+struct ScoreEntry: Comparable, CustomStringConvertible, Equatable {
+    let studentID: Int
+    var points: Int
 
+    var description: String {
+        return "Student: \(studentID) has \(points) points"
+    }
+
+    static func < (lhs: ScoreEntry, rhs: ScoreEntry) -> Bool {
+        lhs.points < rhs.points
+    }
+}
