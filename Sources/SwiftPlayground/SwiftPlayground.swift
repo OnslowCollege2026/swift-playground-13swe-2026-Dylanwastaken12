94e6aacce4fcbde3fc8a48d451d3a3bb14962580
Dylanwastaken12 <142276112+Dylanwastaken12@users.noreply.github.com>
2026-03-26 21:27:26 +0000

    testing time :/

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -21,12 +21,12 @@ import Foundation
         }
 
         static func from(amount: Double) -> Rank {
-            if amount >= 250 { return Rank(label: "S Tier", level: 0) }
-            if amount >= 100 { return Rank(label: "A Tier", level: 1) } 
-            if amount >= 50  { return Rank(label: "B Tier", level: 2) } 
-            if amount >= 25  { return Rank(label: "C Tier", level: 3) } 
-            if amount >= 10  { return Rank(label: "D Tier", level: 4) } 
-            return Rank(label: "F Tier", level: 5) 
+            if amount <= 250 { return Rank(label: "F Tier", level: 0) }
+            if amount <= 100 { return Rank(label: "D Tier", level: 1) } 
+            if amount <= 50  { return Rank(label: "C Tier", level: 2) } 
+            if amount <= 25  { return Rank(label: "B Tier", level: 3) } 
+            if amount <= 10  { return Rank(label: "A Tier", level: 4) } 
+            return Rank(label: "S Tier", level: 5) 
         }
     }
 
@@ -105,12 +105,18 @@ import Foundation
 
                 print("Enter Amount: ", terminator: "")
                 
-                let amountInput = readLine()!
-                let amount = Double(amountInput)! 
+                do {
+                    let amountInput = readLine()!
+                    let amount = Double(amountInput)!
 
-                let newGuest = Guest(name: nameInput, contribution: Contribution(amount: amount))
-                app.guestList.append(newGuest)
-                print("Added \(nameInput).")
+                    let newGuest = Guest(name: nameInput, contribution: Contribution(amount: amount))
+                    app.guestList.append(newGuest)
+                    print("Added \(nameInput).")
+                }
+
+                catch {
+                    print(error)
+                }
             }
 
             app.printTierList()
