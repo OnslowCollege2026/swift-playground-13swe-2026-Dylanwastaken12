e0e02382533292bd72987e1c36d1fb07cdd80c60
Dylanwastaken12 <142276112+Dylanwastaken12@users.noreply.github.com>
2026-04-22 00:58:13 +0000

    Started Assesment

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,9 +1,36 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+import Foundation
+
+/// This Struct contains the books that will be issued out
+///     id: the unique id number for each book
+///     bookTitle: the title of the book
+///     available: a true or false statement showing if the book is available
+///     author: the author of the book
+struct Book: Identifiable, Hashable, Equatable {
+    let id: UUID
+    let bookTitle: String
+    let available: Bool
+    let author: String
+}
+
+
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+        // creates a list of books using the "Book" struct
+        var books = (
+            Book(id: UUID(), bookTitle: "Wool", available: true, author: "Hugh Howey"),
+            Book(id: UUID(), bookTitle: "Shift", available: false, author: "Hugh Howey"),
+            Book(id: UUID(), bookTitle: "BookA", available: true, author: "Dylan"),
+            Book(id: UUID(), bookTitle: "Biography", available: true, author: "Greg"),
+            Book(id: UUID(), bookTitle: "Memoir", available: false, author: "Bernard"),
+            Book(id: UUID(), bookTitle: "BookB", available: true, author: "Oli"),
+            Book(id: UUID(), bookTitle: "BookC", available: true, author: "Theo")
+        )
+        // prints a message to start the programm
+        print("Hello, welcome to the Onslow Book Borrowing System, how may we help you?, A: Add a book to the library, B: ")
     }
 }
+
