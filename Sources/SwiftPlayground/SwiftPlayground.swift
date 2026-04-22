5935d9db4b2feb19c80537572b5b0a9e7ee51f4e
Dylanwastaken12 <142276112+Dylanwastaken12@users.noreply.github.com>
2026-04-22 21:21:15 +0000

    Made structs for books + Requests

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -3,7 +3,7 @@
 
 import Foundation
 
-/// This Struct contains the books that will be issued out
+// This Struct contains the books that will be issued out
 ///     id: the unique id number for each book
 ///     bookTitle: the title of the book
 ///     available: a true or false statement showing if the book is available
@@ -15,10 +15,34 @@ struct Book: Identifiable, Hashable, Equatable {
     let author: String
 }
 
+/// This struct contains the students who will be issuing out the books
+///     id: the id of the student as a UUID
+///     name: the name of the student
+///     age: the age of the student
+struct Student {
+    let id: UUID
+    var name: String
+    var age: Int
+}
+
+
+/// This struct contains each request to issue a book from a student
+///     studentID: the id of the student issuing the book
+///     bookID: the id of the book they are issuing
+struct Request {
+    let studentID: UUID
+    let bookID: UUID
+}
+
+
 
 @main
 struct SwiftPlayground {
     static func main() {
+        
+        // creates a variable that is used for the user's entry
+        var userEntry: String = ""
+        var userChoice: String = ""
         // creates a list of books using the "Book" struct
         var books = (
             Book(id: UUID(), bookTitle: "Wool", available: true, author: "Hugh Howey"),
@@ -30,7 +54,7 @@ struct SwiftPlayground {
             Book(id: UUID(), bookTitle: "BookC", available: true, author: "Theo")
         )
         // prints a message to start the programm
-        print("Hello, welcome to the Onslow Book Borrowing System, how may we help you?, A: Add a book to the library, B: ")
+        print("Hello, welcome to the Onslow Book Borrowing System, how may we help you?, A: Add a book to the library? | B: Register a borrower/User? | C: Print a list of books?")
     }
 }
 
