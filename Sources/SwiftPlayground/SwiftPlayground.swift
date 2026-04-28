94cbe2626fc1d739618c497c3bac60384c639191
Dylanwastaken12 <142276112+Dylanwastaken12@users.noreply.github.com>
2026-04-28 03:07:19 +0000

    Started with adding new students

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -14,7 +14,7 @@ struct Book: Identifiable, Hashable, Equatable, CustomStringConvertible {
     let available: Bool
     let author: String
     var description: String 
-        {"This book is called \(bookTitle), the author is \(author), it's availability is \(available)"}
+        {"| This book is called \(bookTitle), the author is \(author), it's availability is \(available) |"}
 }
 
 /// This struct contains the students who will be issuing out the books.
@@ -25,7 +25,7 @@ struct Student: CustomStringConvertible, Identifiable, Hashable {
     let id: UUID
     var name: String
     var age: Int
-    var description: String {"The sutdent's name is \(name) they are \(age) years old and their id is \(id)"}
+    var description: String {"| The sutdent's name is \(name) they are \(age) years old and their id is \(id) |"}
 }
 
 
@@ -47,15 +47,8 @@ struct Request: Hashable, CustomStringConvertible, Identifiable, Equatable {
 struct SwiftPlayground {
     static func main() {
 
-        // Creates variables that are used for the user's entry.
+        // Creates a variable that is used for the user's entry.
         var userEntry: String = ""
-        var userChoice: String = ""
-
-        var test1: String = "test1"
-        var test2: String = "test2"
-
-        var userName: String = ""
-        var userAge: Int = 0
         // Creates a list of books using the "Book" struct.
         var books = [
             Book(id: UUID(), bookTitle: "Wool", available: true, author: "Hugh Howey"),
@@ -66,20 +59,28 @@ struct SwiftPlayground {
             Book(id: UUID(), bookTitle: "BookB", available: true, author: "Oli"),
             Book(id: UUID(), bookTitle: "BookC", available: true, author: "Theo")
         ]
+
+        // Creates a list of students.
         var students = (
             Student(id: UUID(), name: "Dylan", age: 17)
         )
         // Prints a message to start the program.
         print("Hello, welcome to the Onslow Book Borrowing System, how may we help you?, A: Add a book to the library? | B: Register a borrower/User? | C: Print a list of books? | D: Issue a book to a student?")
         
+        // Aquires user's choice and returns it if it is null.
         guard let userEntry = readLine(), !userEntry.isEmpty else {
             print("please enter a valid input")
             return
         }
+
+        // Code for if the user wants to add a book to the library.
         if userEntry.lowercased() == "a" {
+
+            // Creates variables which are used for user input for title and author.
             let userBookTitle: String = ""
             let userBookAuthor: String = ""
 
+            // Asks user what the title is, value is returned if the input is null, their choice is then printed.
             print("What is the name of the book?")
             guard let userBookTitle = readLine(), !userBookTitle.isEmpty else {
                 print("Book Title is required")
@@ -87,14 +88,59 @@ struct SwiftPlayground {
             }
             print("Your book title is \(userBookTitle)")
 
+            // Asks the user who the author of the book is, 
+            // value is returned if input is null, their choice is then printed.
             print("Who is the author?")
             guard let userBookAuthor = readLine(), !userBookAuthor.isEmpty else {
                 print("Book Author is required")
                 return
             }
+            print("Your author is \(userBookAuthor)")
+
+            // Creates a new instance of the "Book" Struct using the variables aquired from user input,
+            // This instance is assigned to a variable which is added to the list of books.
             var userBookEntry = Book(id: UUID(), bookTitle: userBookTitle, available: true, author: userBookAuthor)
             books.append(userBookEntry)
             print("The new list of available books is \(books)")
+        }
+
+
+
+
+
+
+        if userEntry.lowercased() == "b" {
+            var userAgeInput: String = ""
+            var userName: String = ""
+
+            print("What is the name of the student you're adding?")
+            guard let userName = readLine(), !userName.isEmpty else {
+                print("Name is required")
+                return
+            }
+
+            do {
+                print("How old is the student?")
+                guard let userAgeInput = readLine(), !userAgeInput.isEmpty else {
+                    print("Age is required")
+                    return
+                }
+
+                var userAge = Int(userAgeInput)
+            } catch {
+                print(error)
+
+                var studentEntry = Student(id: UUID(), name: userName, age: userAge)
+            }
+
+            
+
+            print("The new list of students is \(students)")
+
+        }
+
+        if userEntry.lowercased() == "c" {
+            print("List of available books is \(books)")
         }
     }    
 }
