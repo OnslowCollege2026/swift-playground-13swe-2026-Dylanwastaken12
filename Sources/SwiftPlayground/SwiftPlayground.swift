d59cea556d5c425eee710b5d9f6892e516362f4e
Dylanwastaken12 <142276112+Dylanwastaken12@users.noreply.github.com>
2026-05-20 00:30:27 +0000

    Conected to database

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,9 +1,21 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import Foundation
+import GRDB
 
 @main
 struct SwiftPlayground {
     static func main() {
-        
+        let dbPath = "Sources/SwiftPlayground/Flight-Booking.db"
+        do {
+            let dbQueue = try DatabaseQueue(path: dbPath)
+            print("database connection succesful")
+
+            try? dbQueue.read { db in
+                try db.dumpSchema()
+            }
+        } catch {
+            print(error)
+        }
     }
 }
