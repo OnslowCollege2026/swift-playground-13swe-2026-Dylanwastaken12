20cd172f007a08784a7a97a0443848e6b543468f
Dylanwastaken12 <142276112+Dylanwastaken12@users.noreply.github.com>
2026-05-21 22:44:18 +0000

    Finished making struct for all 9 tables

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -84,12 +84,15 @@ struct Location: Identifiable, Codable, FetchableRecord, PersistableRecord {
 
     let id: UUID
 
+    let airportCode: Int
+
     let airportName: String
 
     let airportAdress: String
 
     enum CodingKeys: String, CodingKey {
-        case id = "AirportCode"
+        case id = "id"
+        case airportCode = "AirportCode"
         case airportName = "Airport Name"
         case airportAdress = "Airport Adress"
 
@@ -112,10 +115,85 @@ struct Passenger: Codable, FetchableRecord, PersistableRecord {
         case isMinor = "is Minor"
         case bookingID = "BookingID"
     }
-
-
 }
 
+struct Flight: Codable, FetchableRecord, PersistableRecord {
+
+    let flightID: UUID
+
+    var departureTime: String
+
+    let planeID: UUID
+
+    let departureLocation: String
+
+    let destinationLocation: String
+
+    enum CodingKeys: String, CodingKey {
+        case flightID = "FlightID"
+        case departureTime = "Departure Time"
+        case planeID = "PlaneID"
+        case departureLocation = "Departure Location"
+        case destinationLocation = "Destination Location"
+    }
+}
+
+struct Plane: Codable, FetchableRecord, PersistableRecord {
+
+    let planeID: UUID
+
+    let model: String
+
+    let minRunwayLength: Int
+
+    let maxWeight: Int
+
+    let capacity: Int
+
+    enum CodingKeys: String, CodingKey {
+        case planeID = "PlaneID"
+        case model = "Model"
+        case minRunwayLength = "Min Runway Length"
+        case maxWeight = "Max Weight"
+        case capacity = "Capacity"
+    }
+}
+
+struct Seat: Codable, FetchableRecord, PersistableRecord {
+
+    let seatID: UUID
+
+    let planeID: UUID
+
+    let seatClass: String
+
+    let emergency: Bool
+
+    enum CodingKeys: String, CodingKey {
+        case seatID = "SeatID"
+        case planeID = "PlaneID" 
+        case seatClass = "Seat Class"
+        case emergency = "Emergency"
+    }
+}
+
+struct Runway: Codable, FetchableRecord, PersistableRecord {
+
+    let runwayID: UUID
+    
+    let length: Int
+
+    let airportCode: String
+
+    let type: String
+
+    enum CodingKeys: String, CodingKey {
+        case runwayID = "RunwayID"
+        case length = "Length"
+        case airportCode = "AirportCode"
+        case type = "Type"
+    }
+}
 
 
 @main
